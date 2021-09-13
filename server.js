cc9e477143065b25ff692b4f2921cc39846908ef
Swaroop-RM <swarooprm21@gmail.com>
2021-09-13 16:16:01 +0530

    update new

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/angular-chat-app'));
+app.use(express.static('./dist/angularChatApp'));
 
 app.get('/*', (req, res) =>
-  res.sendFile('index.html', { root: '/dist/angular-chat-app' }),
+  res.sendFile('index.html', { root: '/angularChatApp' }),
 );
 
 // Start the app by listening on the default Heroku port
